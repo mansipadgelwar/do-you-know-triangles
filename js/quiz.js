167dcc796a9e0baaffe55393b7ab7f12f337d323
mansipadgelwar <mpadgelwar@gmail.com>
2021-07-25 14:40:52 +0530

    feat:add  quiz js

---
+++ js/quiz.js
@@ -0,0 +1,25 @@
+var quizForm = document.querySelector("#quiz-form");
+var scoreOutput = document.querySelector("#score-output");
+var btnSubmit = document.querySelector("#btn-submit");
+
+const correctAns = ["option1", "option2", "option1", "option1","option1","option2", "option2", "option3", "option3", "option3" ];
+
+let score = 0;
+
+btnSubmit.addEventListener("click", (e) => {
+
+    e.preventDefault();
+    //console.log("clicked");
+    const data = new FormData(quizForm);    
+    let index=0;
+    for(let entry of data){
+       if(entry[1] == correctAns[index]){
+        
+        score++;
+       }
+        index++;
+        console.log("score");
+    }
+    scoreOutput.innerHTML = score;
+    btnSubmit.style.display= "none";
+});
