c8e7df0da90b718f262a61a374a84c381594cec2
mansipadgelwar <mpadgelwar@gmail.com>
2021-08-01 13:49:18 +0530

    feat: catch error

--- js/area.js
+++ js/area.js
@@ -1,14 +1,18 @@
 var baseLength = document.querySelector(".base");
 var heightLength = document.querySelector(".height");
 var btnCalculate = document.querySelector(".button");
-
+var error = document.querySelector(".error");
 btnCalculate.addEventListener("click",clickHandler);
 
 function clickHandler(e){
     e.preventDefault();
-    const base = Number(baseLength.value);
-    const height = Number(heightLength.value);
-    console.log(base);
-    console.log(height);
-    area.innerText = (base * height)/2;
+    if(baseLength.value > 0 && heightLength.value > 0){
+      const base = Number(baseLength.value);
+      const height = Number(heightLength.value);
+      area.innerText = (base * height)/2;
+    }
+    else{
+        output.style.display = "none";
+        error.innerText = "Enter valid base and height value";
+    }
 }
