fb14d99f0e191d6a6130160c909625a95e08ccc0
mansipadgelwar <mpadgelwar@gmail.com>
2021-09-15 21:27:12 +0530

    refactor: quiz code

--- js/quiz.js
+++ js/quiz.js
@@ -1,17 +1,18 @@
 var quizForm = document.querySelector("#quiz-form");
+var questions = document.querySelectorAll(".question-div");
 var scoreOutput = document.querySelector("#score-output");
 var btnSubmit = document.querySelector("#btn-submit");
 
 const correctAns = ["option1", "option2", "option1", "option1","option1","option2", "option2", "option3", "option3", "option3" ];
 
-btnSubmit.addEventListener("click", (e) => {
+quizForm.addEventListener('submit', (e) => {
     e.preventDefault();
     let score = 0;
+    
     const data = new FormData(quizForm);    
     let index=0;
     for(let entry of data){
-       if(entry[1] == correctAns[index]){
-        
+       if(entry[1] == correctAns[index]){        
         score++;
        }
         index++;
